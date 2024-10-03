36c35c1aaf5e097df7c05ad9dd38b0c88690b319
Beyza Nur Tekerek <beyzanurtekerk@gmail.com>
2024-10-03 22:54:15 +0300

    Initial Commit

---
+++ SwiftUINavigation/SwiftUINavigationApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUINavigationApp.swift
+//  SwiftUINavigation
+//
+//  Created by Beyza Nur Tekerek on 3.10.2024.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUINavigationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
